1f7a8767a076873c6959e5b443f29d1b0f03288a
岳大禹 <yuedayu13@gmail.com>
2015-07-07 16:47:43 +0800

    完成主页

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log("hello World");
+console.log(document.body.clientWidth);
+
+function hasClassName(inElement, inClassName)
+{
+  var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
+  return regExp.test(inElement.className);
+}
+
+function addClassName(inElement, inClassName)
+{
+  if (!hasClassName(inElement, inClassName))
+    inElement.className = [inElement.className, inClassName].join(' ');
+}
+
+function removeClassName(inElement, inClassName)
+{
+  if (hasClassName(inElement, inClassName)) {
+    var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
+    var curClasses = inElement.className;
+    inElement.className = curClasses.replace(regExp, ' ');
+  }
+}
+
+function moveTo(id) {
+  if (document.body.clientWidth <= 600) {
+    addClassName(document.getElementById('menu'), 'hidden');
+    window.scrollTo(0, document.getElementById(id).offsetTop - 40);
+  } else {
+    window.scrollTo(0, document.getElementById(id).offsetTop - 50);
+  }
+}
+
+if (document.body.clientWidth <= 600) {
+  var list = document.getElementById('menu');
+  addClassName(list, 'hidden');
+  document.getElementById('menu-top').onclick = function () {
+    if (hasClassName(list, 'hidden')) {
+      removeClassName(list, 'hidden');
+    } else {
+      addClassName(list, 'hidden');
+    }
+  }
+} else {
+  addClassName(document.getElementById('menu-top'), 'hidden');
+}
